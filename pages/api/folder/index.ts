f11e5cb879ba7d49f40a791192da6ced76eab5ba
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-04 19:09:14 -0300

    fix(db/api): make db and api work with axios return type

--- pages/api/folder/index.ts
+++ pages/api/folder/index.ts
@@ -1,7 +1,7 @@
 import nc from 'next-connect'
 import middleware from '../../../middleware/all'
 import onError from '../../../middleware/error'
-import {  FolderModel } from '../../../db'
+import { FolderModel } from '../../../db'
 import { Request } from '../../../types'
 
 import { NextApiResponse } from 'next'
@@ -12,14 +12,17 @@ const handler = nc({
 
 handler.use(middleware)
 
+handler.get(async (req: Request, res: NextApiResponse) => {
+  const folders = await FolderModel.getFolders(req.db, req.user.id)
+  res.send(folders)
+})
+
 handler.post(async (req: Request, res: NextApiResponse) => {
   const newFolder = await FolderModel.createFolder(req.db, {
     createdBy: req.user.id,
     name: req.body.name,
   })
-
-  res.send({ data: newFolder })
-
+  res.send(newFolder)
 })
 
-export default handler;
+export default handler
