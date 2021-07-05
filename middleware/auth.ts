c0160c360671acf15286987a3e7fd34efe973384
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-04 22:19:57 -0300

    fix(next-auth): add cookie env vars

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,11 @@
 import jwt from 'next-auth/jwt'
 
 export default async (req, res, next) => {
-  const token = await jwt.getToken({ req, secret: process.env.JWT_SECRET })
+  const token = await jwt.getToken({
+    req,
+    secret: process.env.JWT_SECRET,
+    secureCookie: process.env.NEXTAUTH_URL?.startsWith('https://') ?? process.env.VERCEL_ENV === 'preview',
+  })
 
   if (token) {
     // Signed in
